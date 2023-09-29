42099c72469efbf8dffe4ad5a6916a0550370f29
harsh <112680394+itsharshpro@users.noreply.github.com>
2023-09-29 23:26:04 +0530

    Update uniDAO.sol

--- uniDAO.sol
+++ uniDAO.sol
@@ -1,1 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+contract DeadmansSwitch {
+    address public owner;
+    address public beneficiary;
+    uint256 public lastCheckBlock;
+    
+    constructor(address _beneficiary) {
+        owner = msg.sender;
+        beneficiary = _beneficiary;
+        lastCheckBlock = block.number;
+    }
+    
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only the owner can call this function");
+        _;
+    }
+    
+    function stillAlive() external onlyOwner {
+        lastCheckBlock = block.number;
+    }
+    
+    function executeSwitch() external {
+        require(block.number - lastCheckBlock >= 10, "Still alive check not reached 10 blocks");
+        payable(beneficiary).transfer(address(this).balance);
+    }
+}
